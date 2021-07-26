235671d95b237c0140a177a21b93fe2abba4cd38
pshkowda <shkowda@gmail.com>
2021-07-26 09:25:43 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -15,19 +15,21 @@ int main() {
 
     int fileCount = 0;
     int requestedFileCount = 0;
-    std::cout << "Write the requested number of frames (maximum is 5): ";
+    std::cout << "Write the requested number of frames (maximum is 5): "; //to determine requested number of frames
 
     do {        //to prevent too long processes
         std::cin >> requestedFileCount;
-    } while (requestedFileCount > 5);
+    } while (requestedFileCount > 5); //just for test
 
     while (fileCount < requestedFileCount)
     {
         currentDepthFrameRef = camera->getDepthFrame();                       //gets new depth frame
         frameSaver->addNewFrameToBuffer(currentDepthFrameRef);
+
         //proceedToNextMeasurement is true when the current measurement frames have been persisted
 
-        //if (proceedToNextMeasurement) realsenseCameraProvider->spinOnce(); //to get the next frame
+        //if (proceedToNextMeasurement)
+        realsenseCameraProvider->spinOnce(); //to get the next frame
 
         std::list<frame_data> frameBuffer = frameSaver->getFrameDataList();
 
